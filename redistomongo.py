f56efc8fca08f46e0e7b97e1fa282e86c5edbdbe
Costello-13 <74768711+Costello-13@users.noreply.github.com>
2021-05-05 22:25:36 +0200

    Add files via upload

--- redistomongo.py
+++ redistomongo.py
@@ -1,5 +1,7 @@
-import requests, pprint, sched, time, pymongo, redis
+
+import requests, pprint, sched, time, pymongo, redis, json
 from bs4 import BeautifulSoup
+import pandas as pd
 
 mongoclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mongodb = mongoclient["btc_database"]
@@ -8,4 +10,16 @@ mongocol = mongodb["btc_scraper"]
 connection = redis.StrictRedis(host="localhost", port=6379, db=0)
 
 data = connection.get("df")
-print(data)
+
+str_data = data.decode('UTF-8')
+data_json = json.loads(str_data)
+
+data_df = pd.DataFrame(data_json)
+
+#for row in data_df.iterrows():
+	#print(row)
+
+data_df.reset_index(inplace=True)
+data_diccy = data_df.to_dict("records")
+
+mongocol.insert_many(data_diccy)
